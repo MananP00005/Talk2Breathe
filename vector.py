8ce2384839a6c8b41ce2c9a9589e0933408bfac3
Fennerii <jonathanfennerii@gmail.com>
2026-06-03 13:39:39 -0400

    fixes and test data set

--- vector.py
+++ vector.py
@@ -1,36 +1,38 @@
-from langchain_ollama import OllamaEmbeddings  # fixed import
+from langchain_ollama import OllamaEmbeddings
 from langchain_chroma import Chroma
 from langchain_core.documents import Document
 import os
 import pandas as pd
+import shutil
 
-DataFrame = pd.read_csv('SG_Warning.txt', sep='\t', header=None, names=["text"])
-DataFrame["source"] = 'SG_Warning.txt'  # added source column to the DataFrame
+DataFrame = pd.read_csv('Smoking_CSV.csv')
+DataFrame = DataFrame.dropna(subset=['text', 'location'])
 embeddings = OllamaEmbeddings(model="mxbai-embed-large")
 Database_location = "./chroma_db"
-add_documents = not os.path.exists(Database_location)
 
-if add_documents:
-    documents = []
-    ids = []  # renamed from 'id' (id is a built-in Python function)
-    for i, row in DataFrame.iterrows():
-        doc = Document(
-            page_content=row['text'],
-            metadata={"source": row['source']},
-            id=str(i)  # fixed: closed parenthesis was missing
-        )
-        ids.append(str(i))
-        documents.append(doc)
+if os.path.exists(Database_location):
+    shutil.rmtree(Database_location)
+
+documents = []
+ids = []
+for i, row in DataFrame.iterrows():
+    doc = Document(
+        page_content=row['text'],
+        metadata={"source": row['location']},
+        id=str(i)
+    )
+    ids.append(str(i))
+    documents.append(doc)
 
 vector_store = Chroma(
-    collection_name="SG_Warning",
+    collection_name="Smoking_CSV",
     embedding_function=embeddings,
     persist_directory=Database_location
 )
 
-if add_documents:
-    vector_store.add_documents(documents=documents, ids=ids)  # fixed: 'ids' not 'id'
+vector_store.add_documents(documents=documents, ids=ids)
 
 retriever = vector_store.as_retriever(
-    search_kwargs={"k": 3}
+    search_type="mmr",
+    search_kwargs={"k": 5, "fetch_k": 15}
 )
